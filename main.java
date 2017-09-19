6e2b5ec6a70ae36675457b196422042db0fbb8cc
BaptisteMagous <32111279+BaptisteMagous@users.noreply.github.com>
2017-09-19 22:40:20 +0100

    Ajout de Debut et Fin
    Pour bien reperer quand le programme commence et se termine

--- main.java
+++ main.java
@@ -3,13 +3,14 @@
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n Debut");
+		System.out.println("Debut");
 		Couveuse poule = new Couveuse();
 		for(int generation = 1; generation < 1000; generation++) {
 			poule.couver(50, 10, 10);
 		}
 		System.out.println(poule.bestToString());
 		poule.championEnLive();
+		System.out.println("Fin");
 	}
 
 }
